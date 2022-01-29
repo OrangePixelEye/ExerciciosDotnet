9309a121fa7b5cc8e31a18843939313419745130
Felipe Batista <felipe.batistaa52@gmail.com>
2022-01-29 09:16:18 -0300

    feat: complete the unit test

--- ManipulacaoDados.Tests/IntManipulation.cs
+++ ManipulacaoDados.Tests/IntManipulation.cs
@@ -1,17 +1,108 @@
 using Xunit;
 using ManipulacaoDados;
+using System.Linq;
 
 namespace ManipulacaoDados.Tests
 {
 
     public class IntManipulation
     {
+        public void arrBetweenTwoN() {
+            // arrange
+            var m = new ManipuleInt();
+            int n1 = 1;
+            int n2 = 4;
+            int[] expected = new int[]{1, 2, 3, 4};
+
+            // act
+            int[] result = m.arrBetweenTwoN(n1, n2);
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
+        }
+        public void invertArray()
+        {
+            // arrange
+            var m = new ManipuleInt();
+            int[] input = new int[]{3, 2, 1};
+            int[] expected = new int[]{1, 2, 3};
+
+            // act
+            int[] result = m.invertArray(input);
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
+        }
+        public void findPairs() 
+        {
+            // arrange
+            var m = new ManipuleInt();
+            int[] input = new int[]{ 2, 1, 4, 6, 8, 9, 12, 13};
+            int[] expected = new int[]{2, 4, 6, 8, 12};
+
+            // act
+            int[] result = m.findPairs(input);
+            // Assert
+            Assert.True(expected.SequenceEqual(result));        }
+        
+        [Fact]
+        public void uniiqueGreaterThan()
+        {
+            // arrange
+            var m = new ManipuleInt();
+            int n = 6;
+            int[] input = new int[]{ 2, 2, 7, 6, 8, 8, 12, 13};
+            int[] expected = new int[]{7, 8, 12, 13};
+
+            // act
+            int[] result = m.uniqueGreaterThan(input, n);
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
+        }
+        
+        [Fact]
+        public void organize()
+        {
+            // arrange
+            var m = new ManipuleInt();
+            int[] input = new int[]{4,2, 5, 1};
+            int[] expected = new int[]{1, 2, 4, 5};
+
+            // act
+            int[] result = m.organize(input);
+
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
+        }
+
+        [Fact]
+        public void findMultiples() 
+        {
+            // arrange
+            var m = new ManipuleInt();
+            int n = 2;
+            int[] input_arr = new int[]{1, 2, 3, 4, 5, 6};
+            int[] expected = new int[]{2, 4, 6};
+
+            // act
+            int[] result = m.findMultiples(input_arr, n);
+            
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
+        }
+
         [Fact]
         public void SquarePairs()
         {
-            var service = new ManipuleInt();
-            int[] result = service.squareAllPairs(1,4);
-            Assert.Matches(string.Join(' ', result), "4 16");
+            // arrange
+            var m = new ManipuleInt();
+            int n1 = 1;
+            int n2 = 4;
+            int[] expected = new int[]{4, 16};
+
+            // act
+            int[] result = m.squareAllPairs(n1, n2);
+            
+            // Assert
+            Assert.True(expected.SequenceEqual(result));
         }
     }
 }
--- ManipulacaoDados/Program.cs
+++ ManipulacaoDados/Program.cs
@@ -1,9 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] arr = new int[]{1,2,3,4,5,6};
 Console.WriteLine("Hello, World!");
-ManipulacaoDados.ManipuleInt n = new ManipulacaoDados.ManipuleInt();
-///n.squareAllPairs(1, 9);
+// string.Join(' ', result)
+/*int[] t = ManipulacaoDados.ManipuleInt.squareAllPairs(1, 4);
+System.Console.WriteLine(string.Join(' ', t));
+System.Console.WriteLine(t.Count());
 
-
-//n.findMultiples(new int[]{1,2,3,4,5,6},2);
-System.Console.WriteLine();
+int[] a = new int[]{4, 16};
+System.Console.WriteLine(a.Count());
+System.Console.WriteLine(a.SequenceEqual(t));*/
