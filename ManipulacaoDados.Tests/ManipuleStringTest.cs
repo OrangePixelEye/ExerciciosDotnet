7387d1906c2c9196a796ed5042bdab298c95bd94
Felipe Batista <felipe.batistaa52@gmail.com>
2022-01-30 15:44:48 -0300

    feat: implements new tests
    This commit implements some string related tests
    and add some methods' returns

---
+++ ManipulacaoDados.Tests/ManipuleStringTest.cs
@@ -0,0 +1,158 @@
+using Xunit;
+using System.Collections.Generic;
+
+namespace ManipulacaoDados.Tests
+{
+    public class ManipuleStringTest
+    {
+        public void deleteVowels() {
+            // Arrange
+            var m = new ManipuleString();
+            string input = "cool vibes FOR YOU";
+            string expected = "cl vbs FR Y";
+
+            // Act
+            string result = m.deleteVowels(input);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void quantityLetter()
+        {
+            // Arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            char find = 'o';
+            int expected = 2;
+
+            // Act
+            var result = m.quantityLetter(input,find);
+            
+            // Assert
+            Assert.True(result == expected);
+        }
+        public void invertString() 
+        {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            string expected = "sebiv looc";
+
+            // Act
+            string result = m.invertString(input);
+
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void deleteLetter() { 
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            char letter = 'o';
+            string expected = "cl vibes";
+
+            // act
+            string result = m.deleteLetter(input, letter);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void deleteAfterLetter() 
+        {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            char letter = 'v';
+            string expected = "cool ";
+
+            // act
+            string result = m.deleteAfterLetter(input, letter);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void deleteBeforeLetter() 
+        {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            char letter = 'v';
+            string expected = "cool ";
+
+            // act
+            string result = m.deleteBeforeLetter(input, letter);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void toUpperSomeLetters()
+        {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            string letters = "LCBS";
+            string expected = "CooL ViBeS";
+
+            // act
+            string result = m.toUpperSomeLetters(input, letters);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+
+        public void deleteEveryOtherLetter() {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes";
+            
+            string expected = "co ie";
+
+            // act
+            string result = m.deleteEveryOtherLetter(input);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+
+        public void findUniqueLetters() {
+            // arrange
+            var m = new ManipuleString();
+            string input = "cool vibes for you";
+            Dictionary<char,int> expected = new Dictionary<char, int>{
+                {'c', 1},
+                {'o', 4},
+                {'l', 1},
+                {' ', 3},
+                {'v', 1},
+                {'i', 1},
+                {'b', 1},
+                {'e', 1},
+                {'s', 1},
+                {'f', 1},
+                {'r', 1},
+                {'y', 1},
+                {'u', 1}
+            };
+
+            // act
+            Dictionary<char,int> result = m.findUniqueLetters(input);
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+        public void overrideLetter()
+        {
+            // arrange
+            var m = new ManipuleString();
+            string input = "caal vibes far yau";
+            char letterBase = 'a';
+            char overrideTo = 'o';
+            string expected = "CooL ViBeS";
+
+            // act
+            string result = m.overrideLetter(input, letterBase, overrideTo);
+            
+            // Assert
+            Assert.True(expected.Equals(result));
+        }
+    }
+}
--- ManipulacaoDados/class/ManipuleString.cs
+++ ManipulacaoDados/class/ManipuleString.cs
@@ -2,16 +2,25 @@ namespace ManipulacaoDados
 {
     public class ManipuleString
     {
-        public void deleteVowels(string s) {}
-        public void quantityLetter(string s, char find){}
-        public void invertString(string s) {}
-        public void deleteLetter(string s, char l) { }
-        public void deleteAfterLetter(string s, char l) {}
-        public void deleteBeforeLetter(string s, char l) {}
-        public void toUpperSomeLetters(string original, string toUpperLetters){}
-        public void deleteEveryOtherLetter(string s) {}
-        public void howManyLettersIn(string s, char l){}
-        public void findUniqueElementsInArr(string s) {}
-        public void overrideLetter(string s, char baseLetter, char toOverride){}
+        public string deleteVowels(string s) {
+            return string.Empty;
+        }
+        public int quantityLetter(string s, char find)
+        {
+            return 0;
+        }
+        public string invertString(string s) => string.Empty;
+        public string deleteLetter(string s, char l) => string.Empty;
+        public string deleteAfterLetter(string s, char l) => string.Empty;
+        public string deleteBeforeLetter(string s, char l) => string.Empty;
+        public string toUpperSomeLetters(string original, string toUpperLetters) => string.Empty;
+        public string deleteEveryOtherLetter(string s) => string.Empty;
+        
+        // finds unique letters and it quantitty
+        public Dictionary<char, int> findUniqueLetters(string s){
+            return new Dictionary<char, int>();
+        }
+
+        public string overrideLetter(string s, char baseLetter, char toOverride) => string.Empty;
     }
 }
